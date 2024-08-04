568dea8bcd5aa601733ecaf4e99ec198db8b18a3
nekoo7 <abylaiooaooa@mail.ru>
2024-08-04 17:35:01 +0500

    first commit

---
+++ cmd/api/helpers.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) readIDParam(r *http.Request) (int64, error) {
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil || id < 1 {
+		return 0, errors.New("invalid id parameter")
+	}
+	return id, nil
+}
+func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	// Append a newline to make it easier to view in terminal applications
+	js = append(js, '\n')
+	// At this point, we know that we won't encounter any more errors before writing the
+	// response, so it's safe to add any headers that we want to include. W
+
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module greenlight.abylay.net
+
+go 1.22.1
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
