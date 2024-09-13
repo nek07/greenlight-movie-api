66fb2371e2aa5bfb8ee55d231cf15cc465e6ba97
nekoo7 <abylaiooaooa@mail.ru>
2024-09-14 02:36:18 +0500

    sorting and full text search engine

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 	"time"
 
 	"github.com/lib/pq"
@@ -138,12 +139,17 @@ func (m *MovieModel) Delete(id int64) error {
 	return nil
 }
 func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, error) {
-	query := `SELECT id, created_at, title, year, runtime, genres, version FROM movies ORDER BY id`
+	// Use full-text search for the title filter.
+	query := fmt.Sprintf(`SELECT id, created_at, title, year, runtime, genres, version
+			  FROM movies
+			  WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '')
+			  AND (genres @> $2 OR $2 = '{}')
+			  ORDER BY %s %s, id ASC`, filters.sortColumn(), filters.sortDirection())
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	rows, err := m.DB.QueryContext(ctx, query)
+	rows, err := m.DB.QueryContext(ctx, query, title, pq.Array(genres))
 	if err != nil {
 		return nil, err
 	}
