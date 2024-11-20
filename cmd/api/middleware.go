055a69641171479fff753f60126d1a55c115501a
nekoo7 <abylaiooaooa@mail.ru>
2024-11-21 04:32:08 +0500

    preflight cors

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -112,24 +112,27 @@ func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Add the "Vary: Origin" header.
 		w.Header().Add("Vary", "Origin")
-		// Get the value of the request's Origin header.
+		w.Header().Add("Vary", "Access-Control-Request-Method")
 		origin := r.Header.Get("Origin")
-		// Only run this if there's an Origin request header present.
+
 		if origin != "" {
-			// Loop through the list of trusted origins, checking to see if the request
-			// origin exactly matches one of them. If there are no trusted origins, then
-			// the loop won't be iterated.
+
 			for i := range app.config.cors.trustedOrigins {
 				if origin == app.config.cors.trustedOrigins[i] {
-					// If there is a match, then set a "Access-Control-Allow-Origin"
-					// response header with the request origin as the value and break
-					// out of the loop.
+
 					w.Header().Set("Access-Control-Allow-Origin", origin)
+
+					if r.Method == http.MethodOptions && r.Header.Get("Access-Control-Request-Method") != "" {
+						w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, PUT, PATCH, DELETE")
+						w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+
+						w.WriteHeader(http.StatusOK)
+					}
 					break
 				}
 			}
 		}
-		// Call the next handler in the chain.
+
 		next.ServeHTTP(w, r)
 	})
 }
